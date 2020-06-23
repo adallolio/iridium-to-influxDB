72e1914d2d6906793a6f5b195294dcbe53f9cd95
Åsmund Stavdahl <asmund.stavdahl@gmail.com>
2020-06-23 15:33:00 +0200

    handle all defined messages, export CSV file and call csv-to.py

--- iridium_manager.php
+++ iridium_manager.php
@@ -15,7 +15,7 @@ class CSVFile
 
     public function save()
     {
-        file_put_contents("{self::$csvDir}/{$this->filename}", $this->contents);
+        file_put_contents($this->getPath(), "{$this->contents}\n");
     }
 
     public static function fromMatches(array $matches): CSVFile
@@ -34,11 +34,22 @@ class CSVFile
 
         return new CSVFile("{$type}.csv", $csv);
     }
+
+    public function getPath()
+    {
+        return self::$csvDir . "/" . $this->filename;
+    }
 }
 
 if (isset($_GET["debug"])) {
-    $message = "(R) 12:17:42/63.872890,8.640409/b:136/c:98/s:0.00/sat:6/pp:24/cp:6/s:S/001011";
-    #$message = "(ECO) 2020/06/22 09:05:42/63.8729 8.6404/FDOM:393419008.0000/TU:0.0288/CHLA:0.1898";
+    $message =
+    #"(R) 12:17:42/63.872890,8.640409/b:136/c:98/s:0.00/sat:6/pp:24/cp:6/s:S/001011";
+    #"(NAV) 2020-06-23 12:25:21/63.8728 8.6404/C:89/dC:-20/r:-57/th:0/S:0.0/aws:1.7/awd:135";
+    #"(ECO-P) 2020-06-22 09:05:42/63.8729 8.6404/FDOM:393419008.0000/TU:0.0288/CHLA:0.1898";
+    #"(CTD) 2020-06-23 09:37:49/63.8728 8.6404/S:0.29/C:0.04/T:15.80/SS:-1.00/D:0.05";
+    "(OPT-P) 2020-06-23 08:52:44/63.8728 8.6404/T:18.40/AS:98.87/DOX:289.87";
+    #"TBL";
+    #"ADCP";
 } else {
     $json = getIridiumJSON();
     # {"iridium_session_status":0,"momsn":1065,"data":"2852292031313a32383a3334202f2036332035322e3337343337342c382033382e343239303831202f20623a31333620633a31313520733a302e3031207361743a352070703a31372063703a3132202f20733a53202f20303131303131","serial":16978,"iridium_latitude":63.8656,"iridium_cep":4.0,"JWT":"eyJhbGciOiJSUzI1NiJ9.eyJpc3MiOiJSb2NrIDciLCJpYXQiOjE1OTI1NjYxNTMsImRhdGEiOiIyODUyMjkyMDMxMzEzYTMyMzgzYTMzMzQyMDJmMjAzNjMzMjAzNTMyMmUzMzM3MzQzMzM3MzQyYzM4MjAzMzM4MmUzNDMyMzkzMDM4MzEyMDJmMjA2MjNhMzEzMzM2MjA2MzNhMzEzMTM1MjA3MzNhMzAyZTMwMzEyMDczNjE3NDNhMzUyMDcwNzAzYTMxMzcyMDYzNzAzYTMxMzIyMDJmMjA3MzNhNTMyMDJmMjAzMDMxMzEzMDMxMzEiLCJkZXZpY2VfdHlwZSI6IlJPQ0tCTE9DSyIsImltZWkiOiIzMDAyMzQwNjg2Njk0NzAiLCJpcmlkaXVtX2NlcCI6IjQuMCIsImlyaWRpdW1fbGF0aXR1ZGUiOiI2My44NjU2IiwiaXJpZGl1bV9sb25naXR1ZGUiOiI4LjU3OTEiLCJpcmlkaXVtX3Nlc3Npb25fc3RhdHVzIjoiMCIsIm1vbXNuIjoiMTA2NSIsInNlcmlhbCI6IjE2OTc4IiwidHJhbnNtaXRfdGltZSI6IjIwLTA2LTE5IDExOjI5OjEwIn0.fints0W-LysFP5veMWxJeZNN4sScKbqpDJHfLsOzkst-cWMewkNlyt-sxrhyZNSyIgsmdj6sFMQvgHoz_oi8D2OLwlLGyjr2FUqOqvaR8RdSmqT-mUysGBilkCw5Y8TFW8x_BlEZNbxrVUWdCahpcOXjuP_VpYAyBM2m1w_1EbXI25XY5V2T5Fzhtkeavta3unp9Ay1iW5D_OdwVQ0lr437X7HKbkMTgoHz-7QJKt4kYktEC00UN2W9tyY-emiMH68AtwuR6OKvC-jDAK7PC7V9Cu_DtglTGQiuAsoG_7_M4eG59ZEnLb2HMGlN3HKtLcaTIV1Gn-Mqvd-pgcwg6BQ","imei":"300234068669470","device_type":"ROCKBLOCK","transmit_time":"20-06-19 11:29:10","iridium_longitude":8.5791}
@@ -60,34 +71,65 @@ isset($messageType) && error_log("Message type: {$messageType}");
 switch ($messageType) {
     case "R":
         $csvFile = handlePeriodicalReport($message);
+        $csvFilePath = $csvFile->getPath();
+        $table = strtolower($messageType) . "_iridium";
+        $firstCSVLine = explode("\n", $csvFile->contents)[0];
+        $columns = explode(",", $firstCSVLine, 2)[1];
+        $params = "--input {$csvFilePath} --user autonaut --password ntnu_autonaut --dbname AUTONAUT --metricname {$table} --fieldcolumns {$columns}";
         break;
 
-    case "N":
+    case "NAV":
         $csvFile = handleNavigationStatus($message);
+        $csvFilePath = $csvFile->getPath();
+        $table = strtolower($messageType) . "_iridium";
+        $firstCSVLine = explode("\n", $csvFile->contents)[0];
+        $columns = explode(",", $firstCSVLine, 2)[1];
+        $params = "--input {$csvFilePath} --user autonaut --password ntnu_autonaut --dbname AUTONAUT --metricname {$table} --fieldcolumns {$columns}";
         break;
 
     case "CTD":
         $csvFile = handleCTD($message);
+        $csvFilePath = $csvFile->getPath();
+        $table = strtolower($messageType) . "_iridium";
+        $firstCSVLine = explode("\n", $csvFile->contents)[0];
+        $columns = explode(",", $firstCSVLine, 2)[1];
+        $params = "--input {$csvFilePath} --user autonaut --password ntnu_autonaut --dbname AUTONAUT --metricname {$table} --fieldcolumns {$columns}";
         break;
 
     case "ECO":
         $csvFile = handleECO($message);
+        $csvFilePath = $csvFile->getPath();
+        $table = strtolower($messageType) . "_iridium";
+        $firstCSVLine = explode("\n", $csvFile->contents)[0];
+        $columns = explode(",", $firstCSVLine, 2)[1];
+        $params = "--input {$csvFilePath} --user autonaut --password ntnu_autonaut --dbname AUTONAUT --metricname {$table} --fieldcolumns {$columns}";
         break;
 
     case "OPT":
         $csvFile = handleOPT($message);
+        $csvFilePath = $csvFile->getPath();
+        $table = strtolower($messageType) . "_iridium";
+        $firstCSVLine = explode("\n", $csvFile->contents)[0];
+        $columns = explode(",", $firstCSVLine, 2)[1];
+        $params = "--input {$csvFilePath} --user autonaut --password ntnu_autonaut --dbname AUTONAUT --metricname {$table} --fieldcolumns {$columns}";
         break;
 
     case "TBL":
         $csvFile = handleTBL($message);
-        break;
-
-    case "___":
-        $csvFile = handle___($message);
+        $csvFilePath = $csvFile->getPath();
+        $table = strtolower($messageType) . "_iridium";
+        $firstCSVLine = explode("\n", $csvFile->contents)[0];
+        $columns = explode(",", $firstCSVLine, 2)[1];
+        $params = "--input {$csvFilePath} --user autonaut --password ntnu_autonaut --dbname AUTONAUT --metricname {$table} --fieldcolumns {$columns}";
         break;
 
     case "ADCP":
         $csvFile = handleADCP($message);
+        $csvFilePath = $csvFile->getPath();
+        $table = strtolower($messageType) . "_iridium";
+        $firstCSVLine = explode("\n", $csvFile->contents)[0];
+        $columns = explode(",", $firstCSVLine, 2)[1];
+        $params = "--input {$csvFilePath} --user autonaut --password ntnu_autonaut --dbname AUTONAUT --metricname {$table} --fieldcolumns {$columns}";
         break;
 
     default:
@@ -95,7 +137,12 @@ switch ($messageType) {
         break;
 }
 
-#`./csv-to.py {$csvDir}/report.csv $bunchOfStuff`;
+if (isset($csvFile) && $csvFile && isset($params)) {
+    $csvFile->save();
+    #`./csv-to.py {$csvDir}/report.csv $bunchOfStuff`;
+    error_log("python /home/autonaut/java_to_influx/csv-to.py {$params}");
+    `python /home/autonaut/java_to_influx/csv-to.py {$params}`;
+}
 
 function getIridiumJSON(): string
 {
@@ -131,15 +178,15 @@ function handlePeriodicalReport(string $message): CSVFile
 {
     $matches = [];
     preg_match('_^'
-        . '\((?<type>\w+)\) '
+        . '\((?<type>\w+)[^)]*\) '
         . '(?<timestamp>[^/]{8})/'
-        . '(?<lat>[\d. ]+),(?<lon>[\d. ]+)/'
-        . 'b:(?<b>\d+)/'
-        . 'c:(?<c>[-\d]+)/'
-        . 's:(?<s>[\d.]+)/'
-        . 'sat:(?<sat>\d+)/'
-        . 'pp:(?<pp>\d+)/'
-        . 'cp:(?<cp>\d+)/'
+        . '(?<lat>[-\d. ]+),(?<lon>[-\d. ]+)/'
+        . 'b:(?<b>[-\d.]+)/'
+        . 'c:(?<c>[-\d.]+)/'
+        . 's:(?<s>[-\d.]+)/'
+        . 'sat:(?<sat>[-\d.]+)/'
+        . 'pp:(?<pp>[-\d.]+)/'
+        . 'cp:(?<cp>[-\d.]+)/'
         . 's:(?<status>\w)/'
         . '(?<relays>[01]+)'
         . '$_', $message, $matches);
@@ -151,45 +198,48 @@ function handlePeriodicalReport(string $message): CSVFile
 
 function handleNavigationStatus($message): CSVFile
 {
-    $csv = preg_replace(
-        '_^'
-        . '\((?<type>\w+)\) '
-        . '(?<timestamp>[^/]{8})/'
-        . '(?<lat>[\d. ]+),(?<lon>[\d. ]+)/'
-        . 'b:(?<b>\d+)/'
-        . 'c:(?<c>[-\d]+)/'
-        . 's:(?<s>[\d.]+)/'
-        . 'sat:(?<sat>\d+)/'
-        . 'pp:(?<pp>\d+)/'
-        . 'cp:(?<cp>\d+)/'
-        . 's:(?<status>\w)/'
-        . '(?<relays>[01]+)'
-        . '$_',
-        date("Y-m-d") . '${timestamp},'
-        . '${lat},'
-        . '${lon},'
-        . '${b},'
-        . '${c},'
-        . '${s},'
-        . '${sat},'
-        . '${pp},'
-        . '${cp},'
-        . '${status},'
-        . '${relays}',
-        $message);
+    $matches = [];
+    preg_match('_^'
+        . '\((?<type>\w+).*\) '
+        . '(?<timestamp>\d{4}-\d{2}-\d{2} [^/]+)/'
+        . '(?<lat>[-\d.]+) (?<lon>[-\d.]+)/'
+        . 'C:(?<C>[-\d.]+)/'
+        . 'dC:(?<dC>[-\d.]+)/'
+        . 'r:(?<r>[-\d.]+)/'
+        . 'th:(?<th>[-\d.]+)/'
+        . 'S:(?<S>[-\d.]+)/'
+        . 'aws:(?<aws>[-\d.]+)/'
+        . 'awd:(?<awd>[-\d.]+)'
+        . '$_', $message, $matches);
+
+    $matches = getNamedCapturesFromMatches($matches);
+    return CSVFile::fromMatches($matches);
 }
 
 function handleCTD(string $message): CSVFile
 {
-    ;
+    $matches = [];
+    preg_match('_^'
+        . '\((?<type>\w+)[^)]*\) '
+        . '(?<timestamp>\d{4}-\d{2}-\d{2} [^/]+)/'
+        . '(?<lat>[-\d.]+) (?<lon>[-\d.]+)/'
+        . 'S:(?<S>[-\d.]+)/'
+        . 'C:(?<C>[-\d.]+)/'
+        . 'T:(?<T>[-\d.]+)/'
+        . 'SS:(?<SS>[-\d.]+)/'
+        . 'D:(?<D>[-\d.]+)'
+        . '$_', $message, $matches);
+
+    $matches = getNamedCapturesFromMatches($matches);
+    return CSVFile::fromMatches($matches);
 }
 
 function handleECO(string $message): CSVFile
 {
     $matches = [];
     preg_match('_^'
-        . '\((?<type>\w+)\) '
-        . '(?<timestamp>\d{4}/\d{2}/\d{2} [^/]+)/'
+        . '\((?<type>\w+)[^)]*\) '
+        . '(?<timestamp>\d{4}-\d{2}-\d{2} [^/]+)/'
         . '(?<lat>[-\d.]+) (?<lon>[-\d.]+)/'
         . 'FDOM:(?<FDOM>[-\d.]+)/'
         . 'TU:(?<TU>[-\d.]+)/'
@@ -202,17 +252,39 @@ function handleECO(string $message): CSVFile
 
 function handleOPT(string $message): CSVFile
 {
-    ;
+    #(OPT) 2020/06/23 08:51:44/63.8728 8.6404/T:18.40/AS:98.87/DOX:289.87
+    $matches = [];
+    preg_match('_^'
+        . '\((?<type>\w+)[^)]*\) '
+        . '(?<timestamp>\d{4}-\d{2}-\d{2} [^/]+)/'
+        . '(?<lat>[-\d.]+) (?<lon>[-\d.]+)/'
+        . 'T:(?<T>[-\d.]+)/'
+        . 'AS:(?<AS>[-\d.]+)/'
+        . 'DOX:(?<DOX>[-\d.]+)'
+        . '$_', $message, $matches);
+
+    $matches = getNamedCapturesFromMatches($matches);
+    return CSVFile::fromMatches($matches);
 }
 
 function handleTBL(string $message): CSVFile
 {
-    ;
-}
+    #
+    $matches = [];
+    preg_match('_^'
+        . '\((?<type>\w+)[^)]*\) '
+        . '(?<timestamp>\d{4}-\d{2}-\d{2} [^/]+)/'
+        . '(?<lat>[-\d.]+) (?<lon>[-\d.]+)/'
+        . 'SN:(?<SN>[-\d]+)/'
+        . 'T:(?<T>[-\d.]+)/'
+        . 'ANL:(?<ANL>[-\d]+)/'
+        . 'PNL:(?<PNL>[-\d]+)/'
+        . 'RLF:(?<RLF>[-\d]+)/'
+        . 'RMA:(?<RMA>[-\d]+)'
+        . '$_', $message, $matches);
 
-function handle___(string $message): CSVFile
-{
-    ;
+    $matches = getNamedCapturesFromMatches($matches);
+    return CSVFile::fromMatches($matches);
 }
 
 function handleADCP(string $message): CSVFile
